e12b2f5a130b0e813fd0b1bc7bdb29376f22178b
KATTA-00 <kanishkagunawarthana@gmail.com>
2023-06-10 12:36:42 +0530

    backEnd

---
+++ backEnd/LearnLink/src/test/java/com/example/LearnLink/LearnLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LearnLink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
